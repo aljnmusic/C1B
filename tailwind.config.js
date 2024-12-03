8002d8edfb1dbf8dc80bac7ec5a9363d1a09ca3a
aljnmusic <galosjohnaljenne@gmail.com>
2024-12-03 04:22:02 -0800

    welp

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
         sans: ['Roboto', 'sans-serif'],
       }
     },
+    keyframes: {
+      slideDown: {
+        '0%': { transform: 'translateY(-100%)' },
+        '100%': { transform: 'translateY(0)' },
+      },
+    },
+    animation: {
+      slideDown: 'slideDown 2s ease-in-out forwards',
+    },
   },
   plugins: [],
 }
